b2141ed41673d13af64e4fa4605f4effdb46bdef
zhangaixia602 <zhangaixia602@163.com>
2022-02-12 16:33:07 +0800

    更新代码

--- locales/index.js
+++ locales/index.js
@@ -1,9 +1,10 @@
 (function() {
+    console.log(2)
     var currentLang =localStorage.getItem("PC-LANG") || "zh-CN",
     currentLocaleData=currentLang==="zh-CN" ?  window.CNlocalesData: window.ENlocalesData;
     $(".locales-element").each((index, item) => {
             var $item = $(item);
             var tempText = $item.text();
             $item.text(currentLocaleData[tempText] || tempText);
-        });
+    });
 })();
